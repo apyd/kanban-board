7ea417ecdc692920215d98e5f6e38cdd905953ea
apyd <arturpydych@gmail.com>
2025-01-07 17:49:46 +0100

    add prisma client singleton and log boards data as sample

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import prisma from "./lib/prisma";
 
 const app = express();
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, async () => {
   console.log(`Server is running on port: ${process.env.PORT}`);
+
+  const boards = await prisma.board.findMany({});
+  console.log(boards);
 });
