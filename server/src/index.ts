9912f02fd4ba95e10177ce127b33b20f65f03415
apyd <arturpydych@gmail.com>
2025-01-07 17:37:48 +0100

    .env cleanup

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.listen(process.env.SERVER_PORT, () => {
-  console.log(`Server is running on port: ${process.env.SERVER_PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port: ${process.env.PORT}`);
 });
