82121dc8797bf57594a737b40bde2ef9b8221d4f
fukpyd <aniafukas@interia.pl>
2025-02-04 13:10:47 +0100

    update App file and its styles

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,22 @@
 import Header from "@components/Header/Header";
 import { initLocalTheme } from "@utils/initLocalTheme";
+import { ModalMenuProvider } from "./context/ModalMenu/ModalMenu";
+import ModalMenu from "@components/Header/ModalMenu/ModalMenu";
+
+import "./App.scss";
 
 initLocalTheme();
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <ModalMenuProvider>
+      <div className="App">
+        <Header />
+        <div className="modal-wrapper">
+          <ModalMenu />
+        </div>
+      </div>
+    </ModalMenuProvider>
   );
 }
 
