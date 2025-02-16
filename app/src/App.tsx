769fdc0b6aa1aa245105df7eed4991854ba61d4d
fukpyd <aniafukas@interia.pl>
2025-02-16 15:38:14 +0100

    remove unused import

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,8 +3,6 @@ import { initLocalTheme } from "@utils/initLocalTheme";
 import { ModalMenuProvider } from "./context/ModalMenu/ModalMenu";
 import ModalMenu from "@components/Header/ModalMenu/ModalMenu";
 
-import "./App.scss";
-
 initLocalTheme();
 
 function App() {
