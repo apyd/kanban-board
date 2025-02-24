9d2adcbd31495dcc4b4dd67a7ba2715804f11788
fukpyd <aniafukas@interia.pl>
2025-02-24 19:04:54 +0100

    update useTheme, remove unused code

--- app/src/hooks/useTheme.ts
+++ app/src/hooks/useTheme.ts
@@ -1,18 +1,10 @@
 import { useContext, useEffect } from "react";
 import ThemeToggleContext from "@context/ThemeToggle/ThemeToggle";
 
-type Theme = [() => void, string];
+type Theme = () => void;
 
 const useTheme = (): Theme => {
   const themeToggleCtx = useContext(ThemeToggleContext);
-  // const localStorageTheme = localStorage.getItem("theme") as string;
-  // const [theme, setTheme] = useState<string>(localStorageTheme);
-
-  // const toggleTheme = () => {
-  //   const updatedTheme = theme === "light" ? "dark" : "light";
-  //   setTheme(updatedTheme);
-  //   document.documentElement.setAttribute("data-theme", updatedTheme);
-  // };
 
   useEffect(() => {
     const themeRefresh = () => {
