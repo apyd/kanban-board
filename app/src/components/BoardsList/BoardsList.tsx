9dbd7e9ad419d2231d05c2235a3f701c074657ca
fukpyd <aniafukas@interia.pl>
2025-03-24 13:49:55 +0100

    update BoardList component, including implmentaton of scss module

--- app/src/components/BoardsList/BoardsList.tsx
+++ app/src/components/BoardsList/BoardsList.tsx
@@ -1,45 +1,40 @@
-import { useState } from "react";
-import { BoardItem } from "./BoardsList.types";
-import "@components/BoardsList/BoardsList.scss";
+import { useState, useContext } from "react";
+import styles from "@components/BoardsList/BoardsList.module.scss";
 import BoardIcon from "@assets/icons/board.svg?react";
 import PlusIcon from "@assets/icons/plus.svg?react";
 import Button from "@components/ui/Button/Button";
+import BoardModalContext from "@context/BoardModal/BoardModal";
+import BoardsContentContext from "@context/BoardsContent/BoardsContent";
 
 const BoardsList = () => {
-  const [boards, setBoards] = useState<BoardItem[]>([]);
-  const [activeElementId, setActiveElementId] = useState<number>();
+  const { boardsContent: boards } = useContext(BoardsContentContext);
+  const { openBoardModal } = useContext(BoardModalContext);
+  const [activeElementId, setActiveElementId] = useState<string>();
 
-  const handleAddBoard = () => {
-    setBoards((prevBoard) => {
-      return [
-        ...prevBoard,
-        { title: "Marketing Plan", id: prevBoard.length + 1 },
-      ];
-    });
-  };
-
-  const handleActiveElement = (id: number) => {
-    const activeElement = boards.find((board) => board.id === id);
-    setActiveElementId(activeElement?.id);
+  const handleActiveElement = (boardId: string) => {
+    const activeElement = boards.find((board) => board.boardId === boardId);
+    setActiveElementId(activeElement?.boardId);
   };
 
   return (
-    <section className="boards">
-      <header className="boards-header">
-        <h2 className="boards-heading">ALL BOARDS ({boards.length})</h2>
+    <section className={styles["boards"]}>
+      <header className={styles["boards-header"]}>
+        <h2 className={styles["boards-heading"]}>
+          ALL BOARDS ({boards.length})
+        </h2>
       </header>
       {boards.length > 0 && (
-        <ul className="boards-list">
+        <ul className={styles["boards-list"]}>
           {boards.map((board) => {
             return (
-              <li key={board.id} className="boards-list-item">
+              <li key={board.boardId} className={styles["boards-list-item"]}>
                 <Button
                   variant="primary"
                   rounded="right-rounded"
-                  label={board.title}
-                  current={activeElementId === board.id}
-                  Icon={<BoardIcon className="board-icon" />}
-                  onClick={() => handleActiveElement(board.id)}
+                  label={board.boardTitle}
+                  current={activeElementId === board.boardId}
+                  Icon={<BoardIcon className={styles["board-icon"]} />}
+                  onClick={() => handleActiveElement(board.boardId)}
                   as="a"
                   href="#"
                 ></Button>
@@ -53,8 +48,8 @@ const BoardsList = () => {
         rounded="right-rounded"
         label="Create New Board"
         withStaticStyles
-        Icon={<PlusIcon className="plus-icon" />}
-        onClick={handleAddBoard}
+        Icon={<PlusIcon className={styles["plus-icon"]} />}
+        onClick={openBoardModal}
       ></Button>
     </section>
   );
