e0bf996c17853a68c4034118ac34f24caf6101cc
fukpyd <aniafukas@interia.pl>
2025-03-30 13:08:35 +0200

    add TaskModalProvider and NewTask component to App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,29 +3,34 @@ import { ModalMenuProvider } from "@context/ModalMenu/ModalMenu";
 import { ThemeToggleProvider } from "@context/ThemeToggle/ThemeToggle";
 import { BoardsContentProvider } from "@context/BoardsContent/BoardsContent";
 import { BoardModalProvider } from "@context/BoardModal/BoardModal";
+import { TaskModalProvider } from "@context/TaskModal/TaskModal";
 import Header from "@components/Header/Header";
 import Sidebar from "@components/Sidebar/Sidebar";
 import ModalMenu from "@components/Header/ModalMenu/ModalMenu";
 import ModalBoard from "@components/ModalBoard/ModalBoard";
+import NewTask from "@components/NewTask/NewTask";
 
 initLocalTheme();
 
 function App() {
   return (
-    <BoardModalProvider>
-      <BoardsContentProvider>
-        <ThemeToggleProvider>
-          <ModalMenuProvider>
-            <div className="App">
-              <Header />
-              <Sidebar />
-              <ModalMenu />
-              <ModalBoard />
-            </div>
-          </ModalMenuProvider>
-        </ThemeToggleProvider>
-      </BoardsContentProvider>
-    </BoardModalProvider>
+    <TaskModalProvider>
+      <BoardModalProvider>
+        <BoardsContentProvider>
+          <ThemeToggleProvider>
+            <ModalMenuProvider>
+              <div className="App">
+                <Header />
+                <Sidebar />
+                <ModalMenu />
+                <ModalBoard />
+                <NewTask />
+              </div>
+            </ModalMenuProvider>
+          </ThemeToggleProvider>
+        </BoardsContentProvider>
+      </BoardModalProvider>
+    </TaskModalProvider>
   );
 }
 
