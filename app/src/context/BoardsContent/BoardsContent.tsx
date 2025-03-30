f5b2ecc962cb526c317f6b4ce70c3daaef2a66e6
fukpyd <aniafukas@interia.pl>
2025-03-30 13:04:35 +0200

    update BoardContent context

--- app/src/context/BoardsContent/BoardsContent.tsx
+++ app/src/context/BoardsContent/BoardsContent.tsx
@@ -4,15 +4,27 @@ import { Column } from "src/shared/types";
 import { v4 as uuidv4 } from "uuid";
 
 const BoardsContentContext = createContext<{
-  boardsContent: BoardContent[];
+  boardsContent: BoardContent[]; // [{boardTitle: x, boardColumns: [{id: a, value: b, tasks: [{taskName: c, taskId: d, taskDescription: e, subtasks: [{subtaskName: f, subtaskId: g}]}]}], boardId: y}]
   handleAddBoard: (boardTitle: string, boardColumns: Column[]) => void;
+  handleActiveBoard: (boardId: string) => void;
+  activeBoardId: string | undefined;
+  activeBoard: BoardContent;
 }>({
   boardsContent: [],
   handleAddBoard: () => {},
+  handleActiveBoard: () => {},
+  activeBoardId: "",
+  activeBoard: { boardTitle: "", boardColumns: [], boardId: "" },
 });
 
 export const BoardsContentProvider = ({ children }: BoardsContentContext) => {
   const [boardsContent, setBoardsContent] = useState<BoardContent[]>([]);
+  const [activeBoardId, setActiveBoardId] = useState<string>();
+  const [activeBoard, setActiveBoard] = useState<BoardContent>({
+    boardTitle: "",
+    boardColumns: [],
+    boardId: "",
+  });
 
   const handleAddBoard = (boardTitle: string, boardColumns: Column[]) => {
     setBoardsContent((prevBoardsContent) => {
@@ -35,9 +47,24 @@ export const BoardsContentProvider = ({ children }: BoardsContentContext) => {
     });
   };
 
+  const handleActiveBoard = (boardId: string) => {
+    const activeBoard = boardsContent.find(
+      (board) => board.boardId === boardId
+    );
+
+    if (activeBoard) {
+      setActiveBoard(activeBoard);
+      setActiveBoardId(activeBoard?.boardId);
+    }
+    console.log(activeBoard);
+  };
+
   const boardsContentCtx = {
     boardsContent,
     handleAddBoard,
+    handleActiveBoard,
+    activeBoardId,
+    activeBoard,
   };
 
   return (
